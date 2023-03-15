dbbd18a9b1a08a3934a489eeb29bc032e52b2acb
SuyoungJung <tdhitbtd@gmail.com>
2023-03-15 13:30:28 +0900

    자바 기초 11강 과제 0315

--- src/com/java09/Main2.java
+++ src/com/java09/Main2.java
@@ -3,5 +3,7 @@ package com.java09;
 public class Main2 {
     public static void main(String[] args) {
         PoisonKinoko poisonkinoko = new PoisonKinoko('A');
+        SuperHero superHero = new SuperHero();
+        poisonkinoko.attack(superHero);
     }
 }
--- src/com/java09/PoisonKinoko.java
+++ src/com/java09/PoisonKinoko.java
@@ -3,6 +3,8 @@ package com.java09;
 public class PoisonKinoko extends Kinoko {
     private int poisonCount = 5;
     
+    // getPoisonCount getter&setter는 내부의 작업이니까 안써도 됨 
+    
     public PoisonKinoko(char suffix) {
         super(suffix);
         // TODO Auto-generated constructor stub
@@ -11,7 +13,7 @@ public class PoisonKinoko extends Kinoko {
     public void attack(Hero hero) {
         // TODO Auto-generated method stub
         super.attack(hero);
-        if ( poisonCount != 0) {
+        if (poisonCount != 0) {
             System.out.println("추가로, 독 포자를 살포했다!");
             int beforeAttackHp = hero.getHp();
             hero.setHp(beforeAttackHp * 4 / 5);
