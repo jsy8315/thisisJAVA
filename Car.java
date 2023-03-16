ee1930e0e72af243d4cfcf02a5195402533264b2
SuyoungJung <tdhitbtd@gmail.com>
2023-03-16 16:59:28 +0900

    다형성 예제 추가

--- Car.java
+++ Car.java
@@ -2,7 +2,7 @@ package com.javabookCh07;
 
 public class Car {
     // 필드 선언
-    public Tire tire;
+    public Tire tire; // Tire 클래스의 객체를 참조하기 위한 변수 tire를 선언함
     
     // 메소드 선언
     public void run() {
